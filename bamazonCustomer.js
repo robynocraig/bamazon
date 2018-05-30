0369d079e5788ee375bbe972f8ba1b3d8f82a659
robynocraig <robynocraig@gmail.com>
2018-05-29 21:38:08 -0500

    added logic to display all items when app first runs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,5 +22,19 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  //console.log("connected as id " + connection.threadId);
+  afterConnection();
 });
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item ID: " + res[i].item_id + " || " + "Product: " + res[i].product_name + " || " + "Department: " + res[i].department_name + " || " + "Price: $" + res[i].price);
+      console.log("------------------");
+    }
+
+    connection.end();
+  });
+}
