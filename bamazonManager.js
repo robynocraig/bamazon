dec017c4b23b1c9bade6842307311a56d2d3cb18
robynocraig <robynocraig@gmail.com>
2018-06-02 21:44:19 -0500

    added view low inventory and add to inventory functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -74,3 +74,86 @@ function viewProducts() {
 
   });
 }
+
+function lowInventory() {
+
+  // displays all items
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].stock_quantity < 5) {
+        console.log(res[i].product_name + ": " + res[i].stock_quantity);
+      }
+    }
+
+    // runs prompt function
+    managerActions();
+
+  });
+}
+
+// update the quantity in mysql with items purchased
+function addInventory() {
+  inquirer
+    .prompt([
+      {
+        name: "userId",
+        type: "input",
+        message: "What is the item ID you would like to add inventory to?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to add?"
+      },
+    ])
+    .then(function(answer) {
+      // getting information from the item ID the user inputed
+      connection.query("SELECT * FROM products", function(err, res) {
+
+        if (err) throw err;
+
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].item_id == answer.userId) {
+            chosenItem = res[i];
+          }
+        }
+
+        //console.log("Chosen Item: " + chosenItem.product_name);
+        var updatedStock = (parseFloat(chosenItem.stock_quantity) + parseFloat(answer.quantity))
+        console.log("Updated Stock: " + updatedStock);
+        updateQuantity();
+
+        // update the quantity in mysql with items purchased
+        function updateQuantity() {
+          var query = connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: updatedStock
+              },
+              {
+                item_id: answer.userId
+              }
+            ],
+          );
+        }
+
+        // runs prompt function
+        managerActions();
+
+    });
+  });
+}
+
+function newProduct() {
+
+    // runs prompt function
+    managerActions();
+
+  });
+}
+
+// See if a Quit option can be added that quits the app
